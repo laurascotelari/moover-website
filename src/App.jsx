d39b06e8b9d9c1db358a66f6603a1dcb22599c39
laurascotelari <laura.scotelari@usp.br>
2024-11-12 16:22:47 -0300

    Configurando tailwind com as cores e fontes definidas no figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Loja Mooover
-    </h1>
-  )
-}
+const App = () => (
+    <main className="relative">
+      <h1 className="text-3xl font-bold underline">
+        Loja Moover
+      </h1>
+    </main>
+  );
+
+  export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export default{
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary-lighter': "#EBC059",
+        "primary-darker": "#4B330C",
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
   },
   plugins: [],
 }
